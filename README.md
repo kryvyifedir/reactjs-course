fb1d8051f00eb09d7fbc65971ecc7aa29792187a
Fedir Kryvyi <fedirkryvyi@outlook.com>
2023-04-10 22:36:33 +0200

    part03 - task 3.9-3.11 Adding stuff for fly.io deployment

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Personal study of React JS
 
 Course Link: https://fullstackopen.com/en/about
+
+Link to the App in the Fly.IO
+https://aged-water-4325.fly.dev/
+
+Kill local ports
+lsof -i :<PortNumber>
+kill -9 <PID>
--- part03/task01-08/index.js
+++ part03/task01-08/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
   return [
@@ -108,7 +111,7 @@ app.post('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
